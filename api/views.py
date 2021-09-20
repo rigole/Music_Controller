de8e5f5877373767ceca6bc282dbc113fc21b4a6
rigole <foplacide@gmail.com>
2021-09-20 14:34:08 +0100

    creating serialisations using REST API of our data that are going to be used by the front end

--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
 
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
