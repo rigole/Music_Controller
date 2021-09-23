877b877faaa1bb1f814f1b6d492c98c093a90482
rigole <foplacide@gmail.com>
2021-09-23 21:35:34 +0100

    setting the serializers to create room and the model to generate a unique code by default

--- api/views.py
+++ api/views.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponse
-from rest_framework import generics
-from .serializers import RoomSerializer
+from rest_framework import generics, status
+from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
 
 class RoomView(generics.CreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        pass
