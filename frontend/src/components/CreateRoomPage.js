363230727452819e6c7d41f0d592de9670b39147
rigole <foplacide@gmail.com>
2021-09-24 23:20:29 +0100

    creating grid and button to create room and back

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
 import TextField from "@material-ui/core/TextField";
@@ -34,8 +34,49 @@ export default class CreateRoomPage extends Component {
                             <FormHelperText>
                                 <div align="center">Guest Control of Playback State</div>
                             </FormHelperText>
+                            <RadioGroup row defaultValue="true">
+                                <FormControlLabel
+                                    value="true"
+                                    control={<Radio color="primary"/>}
+                                    label="Play/Pause"
+                                    labelPlacement="bottom"
+                                />
+                                <FormControlLabel
+                                    value="false"
+                                    control={<Radio color="secondary"/>}
+                                    label="No Control"
+                                    labelPlacement="bottom"
+
+                                />
+                            </RadioGroup>
                         </FormControl>
 
+                </Grid>
+                    <Grid item xs={12} align="center">
+                        <FormControl>
+                            <TextField
+                                required={true}
+                                type="number"
+                                defaultValue={this.defaultVotes}
+                                inputProps={{
+                                    min:1,
+                                    style: { textAlign: "center" },
+                                }}
+                            />
+                            <FormHelperText>
+                                <div align="center">Votes Required to Skip Song</div>
+                            </FormHelperText>
+                        </FormControl>
+                </Grid>
+                  <Grid item xs={12} align="center">
+                    <Button color="primary" variant="contained" >
+                       Create Room
+                    </Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button color="secondary" variant="contained" to="/" component={Link}>
+                        Back
+                    </Button>
                 </Grid>
             </Grid>
         )
