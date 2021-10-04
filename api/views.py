c736afa99ad48113af99feb28c09b50b85b9fa47
rigole <foplacide@gmail.com>
2021-10-04 09:59:09 +0100

    designing the room component

--- api/views.py
+++ api/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 # Create your views here.
 
 
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Grid, Button, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default class Room extends Component{
     constructor(props) {
@@ -25,13 +27,34 @@ export default class Room extends Component{
     }
 
     render() {
-        return(
-            <div>
-                <h1>{this.roomCode}</h1>
-                <p>Votes: {this.state.votesToSkip}</p>
-                <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
-                <p>Host: {this.state.isHost.toString()}</p>
-            </div>
-        )
+        return (
+                  <Grid container spacing={1}>
+                      <Grid item xs={12} align="center">
+                          <Typography variant="h4" component="h4">
+                                Code: {this.roomCode}
+                          </Typography>
+                      </Grid>
+                       <Grid item xs={12} align="center">
+                            <Typography variant="h6" component="h6">
+                                Votes: { this.state.votesToSkip }
+                          </Typography>
+                       </Grid>
+                       <Grid item xs={12} align="center">
+                           <Typography variant="h6" component="h6">
+                            Guest Can Pause: {this.state.guestCanPause.toString()}
+                           </Typography>
+                       </Grid>
+                       <Grid item xs={12} align="center">
+                           <Typography variant="h6" component="h6">
+                            Host: {this.state.isHost.toString()}
+                           </Typography>
+                       </Grid>
+                      <Grid item xs={12} align="center">
+                           <Button variant="contained" color="secondary" to="/" component={Link}>
+                               Leave Room
+                           </Button>
+                       </Grid>
+                  </Grid>
+            )
     }
 }
